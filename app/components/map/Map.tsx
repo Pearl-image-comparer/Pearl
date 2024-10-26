4354f708a57658d929462458dfcc54f2f7f9395c
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-10-26 20:48:53 +0300

    feat(map): create lazy-loaded leaflet map component

---
+++ app/components/map/Map.tsx
@@ -0,0 +1,13 @@
+import { lazy, Suspense } from "react";
+
+// Lazy loading to ensure the browser environment is available before render.
+const LazyMap = lazy(() => import("./_MapComponent.client"));
+
+export default function Map(): JSX.Element {
+    return (
+        //TODO: Add proper fallback
+        <Suspense fallback={<p>Lazy loading...</p>}>
+            <LazyMap />
+        </Suspense>
+    )
+}
---
+++ app/components/map/_MapComponent.client.tsx
@@ -0,0 +1,28 @@
+/*
+WARNING: This file is an internal component and should not be imported
+outside of the `components/map` -folder. Use `Map.tsx` instead.
+
+Note: The filename must include "client" for the Leaflet map to work
+correctly because Remix uses server-side rendering. This requires the
+component to be rendered on the client side so that Leaflet can access
+the window object and DOM elements.
+*/
+
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css"
+
+export default function MapComponent(): JSX.Element {
+    const center: L.LatLngExpression = [61.4978, 23.761];
+
+    return (
+        <div className="map" style={{ width: "100%", height: "100vh" }}>
+            <MapContainer center={center} zoom={13} style={{ width: "100%", height: "100%" }}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={center} />
+            </MapContainer>
+        </div>
+    );
+}
