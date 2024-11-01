f94c2bdc114f0e158b2586f4b733277e4d91a05f
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-11-01 19:48:11 +0200

    style: run prettier

--- app/components/controls/Controls.tsx
+++ app/components/controls/Controls.tsx
@@ -3,10 +3,10 @@ import SearchBar from "./searchbar/SearchBar";
 import Fabs from "./fabs/Fabs";
 
 export default function Controls() {
-    return (
-        <Container>
-            <SearchBar />
-            <Fabs />
-        </Container>
-    );
+  return (
+    <Container>
+      <SearchBar />
+      <Fabs />
+    </Container>
+  );
 }
--- app/components/controls/searchbar/SearchBar.tsx
+++ app/components/controls/searchbar/SearchBar.tsx
@@ -1,36 +1,31 @@
-import { IconButton, InputBase, Paper} from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
+import { IconButton, InputBase, Paper } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function SearchBar() {
+  // TODO: Properly good to separate styling to it's own file
+  const paperStyle = {
+    p: "0.1rem 0.2rem 0.1rem 0.6rem",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1000,
+    position: "absolute",
+    top: "0.7rem",
+    left: "0.7rem",
+    right: "0.7rem",
+  };
 
-    // TODO: Properly good to separate styling to it's own file
-    const paperStyle = {
-      p: "0.1rem 0.2rem 0.1rem 0.6rem",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      zIndex: 1000,
-      position: "absolute",
-      top: "0.7rem",
-      left: "0.7rem",
-      right: "0.7rem"
-    };
+  const inputBaseStyle = {
+    ml: 1,
+    flex: 1,
+  };
 
-    const inputBaseStyle = {
-        ml: 1, flex: 1
-    };
-
-    return (
-        <Paper sx={paperStyle}>
-            <InputBase style={inputBaseStyle} placeholder="Search for a place" />
-            <IconButton type="button">
-            <SearchIcon />
-            </IconButton>
-        </Paper>
-    );
-
-
-
-
-
+  return (
+    <Paper sx={paperStyle}>
+      <InputBase style={inputBaseStyle} placeholder="Search for a place" />
+      <IconButton type="button">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
 }
