682dd1a30302bae76d664d02f07de8fb395087a5
unknown <jonttis@gmail.com>
2024-11-09 17:29:20 +0200

    feat(map): add functionality to viewCentered -button

--- app/components/controls/fabs/Fabs.tsx
+++ app/components/controls/fabs/Fabs.tsx
@@ -5,11 +5,13 @@ import AddIcon from "@mui/icons-material/Add";
 import LocationSearchingIcon from "@mui/icons-material/LocationSearching";
 import { useTheme, Theme } from "@mui/material/styles";
 import MyLocationIcon from "@mui/icons-material/MyLocation";
+import { useMap } from "react-leaflet";
 
 export default function Fabs() {
   const [viewCentered, setViewCentered] = useState(false);
   const [satelliteViewOpen, setSatelliteViewOpen] = useState(false);
   const theme: Theme = useTheme();
+  const map = useMap();
 
   const StyledStack = styled(Stack)({
     display: "flex",
@@ -45,21 +47,30 @@ export default function Fabs() {
       backgroundColor: theme.palette.background.paper,
     },
   });
-
-  const handleViewCentered = () => {
-    setViewCentered(!viewCentered);
-  };
-
+    
   const handleSatelliteView = () => {
-    setSatelliteViewOpen(!satelliteViewOpen);
-  };
+    setSatelliteViewOpen(!satelliteViewOpen); 
+  }; 
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition( 
+        (position) => {
+           const { latitude, longitude } = position.coords; 
+           map.flyTo([latitude, longitude], 16);
+           map 
+           }, (err) => { console.error(err.message); }
+           ); 
+          } else { 
+            console.error("Geolocation is not supported by this browser."); } };
 
   return (
     <StyledStack direction="column">
       <StyledToggleButton
         value="check"
         selected={viewCentered}
-        onChange={handleViewCentered}
+        onChange={() => {
+          getLocation();
+        }}
       >
         {viewCentered ? <MyLocationIcon /> : <LocationSearchingIcon />}
       </StyledToggleButton>
