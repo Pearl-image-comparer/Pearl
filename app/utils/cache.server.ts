57e1d12562136213083ecfa748bd2d83a76e4bc8
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2024-11-12 20:40:04 +0200

    refactor(style): run prettier

--- app/utils/cache.server.ts
+++ app/utils/cache.server.ts
@@ -1,7 +1,9 @@
 import { Keyv } from "keyv";
 import { KeyvMemcache } from "@keyv/memcache";
 
-const memcache = new KeyvMemcache(process.env.MEMCACHE_HOST ?? "localhost:11211");
+const memcache = new KeyvMemcache(
+  process.env.MEMCACHE_HOST ?? "localhost:11211",
+);
 const keyv = new Keyv({ store: memcache });
 
 export default keyv;
