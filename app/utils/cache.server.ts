8df3e7350431d0e94e5e7f3cb660982c5e20471f
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2024-11-12 19:56:19 +0200

    feat(cache): configure memcache host

--- app/utils/cache.server.ts
+++ app/utils/cache.server.ts
@@ -1,8 +1,7 @@
 import { Keyv } from "keyv";
 import { KeyvMemcache } from "@keyv/memcache";
 
-// TODO: Configure cache server URI.
-const memcache = new KeyvMemcache("localhost:11211");
+const memcache = new KeyvMemcache(process.env.MEMCACHE_HOST ?? "localhost:11211");
 const keyv = new Keyv({ store: memcache });
 
 export default keyv;
