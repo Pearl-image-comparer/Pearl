a62d2df09f891cec1cbc2708bb83bcb4794f56ee
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2024-11-14 15:46:36 +0200

    feat(map): side-by-side view opacity

--- app/components/map/comparison/SideBySide.tsx
+++ app/components/map/comparison/SideBySide.tsx
@@ -6,9 +6,16 @@ import { useMap } from "react-leaflet";
 interface SideBySideProps {
   leftDate: string;
   rightDate: string;
+  opacity: number;
+  onTop: "left" | "right";
 }
 
-export default function SideBySide({ leftDate, rightDate }: SideBySideProps) {
+export default function SideBySide({
+  leftDate,
+  rightDate,
+  opacity = 1.0,
+  onTop = "left",
+}: SideBySideProps) {
   const map = useMap();
 
   useEffect(() => {
@@ -19,6 +26,8 @@ export default function SideBySide({ leftDate, rightDate }: SideBySideProps) {
       // @ts-expect-error Time is valid but not included in the type definition.
       time: leftDate,
       transparent: true,
+      opacity: onTop === "left" ? opacity : undefined,
+      zIndex: onTop === "left" ? 100 : undefined,
     });
 
     const rightLayer = L.tileLayer.wms("/wms", {
@@ -28,9 +37,33 @@ export default function SideBySide({ leftDate, rightDate }: SideBySideProps) {
       // @ts-expect-error Time is valid but not included in the type definition.
       time: rightDate,
       transparent: true,
+      opacity: onTop === "right" ? opacity : undefined,
+      zIndex: onTop === "right" ? 100 : undefined,
     });
 
     const sideBySideControl = L.control.sideBySide(leftLayer, rightLayer);
+
+    // @ts-expect-error This is a really weird hack but works nonetheless.
+    const updateClip = sideBySideControl._updateClip.bind(sideBySideControl);
+    // @ts-expect-error The hack continues.
+    sideBySideControl._updateClip = () => {
+      // Call the original "_updateClip()".
+      updateClip();
+
+      if (onTop === "left") {
+        // @ts-expect-error Accessing "private" variables.
+        if (sideBySideControl._rightLayer) {
+          // @ts-expect-error And unset it's clip rect.
+          sideBySideControl._rightLayer.getContainer().style.clip = "unset";
+        }
+      } else if (onTop === "right") {
+        // @ts-expect-error Accessing "private" variables.
+        if (sideBySideControl._leftLayer) {
+          // @ts-expect-error And unset it's clip rect.
+          sideBySideControl._leftLayer.getContainer().style.clip = "unset";
+        }
+      }
+    };
 
     // Add controls and layers to the map
     sideBySideControl.addTo(map);
@@ -43,7 +76,7 @@ export default function SideBySide({ leftDate, rightDate }: SideBySideProps) {
       map.removeLayer(leftLayer);
       map.removeLayer(rightLayer);
     };
-  }, [map, leftDate, rightDate]);
+  }, [map, leftDate, rightDate, onTop, opacity]);
 
   return null;
 }
