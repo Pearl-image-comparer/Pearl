e1f10e6bcb19cb6cdec259e2aaca344e09a89129
unknown <jonttis@gmail.com>
2024-11-15 17:27:18 +0200

    refactor: add input sanitization, return data directly and remove unnecessary catch

--- app/routes/lajidata.tsx
+++ app/routes/lajidata.tsx
@@ -9,32 +9,50 @@ export const loader: LoaderFunction = async ({ request }) => {
     console.error("Missing bounds or access token.");
     return json(
       { data: [], message: "Missing required bounds or access token" },
-      { status: 400 },
+      { status: 400 }
     );
+  }
+  if (!accessToken) {
+    console.error("Missing access token");
+    return json({ data: [], message: "Missing access token" }, { status: 500 });
   }
 
   const [southLat, northLat, westLng, eastLng] = bounds.split(":");
 
-  try {
-    const response = await fetch(
-      `https://api.laji.fi/v0/warehouse/query/unit/list?wgs84CenterPoint=${southLat}:${northLat}:${westLng}:${eastLng}:WGS84&redListStatusId=MX.iucnEN,MX.iucnCR,MX.iucnVU,MX.iucnNT&pageSize=10&access_token=${accessToken}`,
-    );
-
-    if (!response.ok) {
-      console.error("Network response was not ok.");
-      throw new Error("Failed to fetch data from Laji API");
-    }
-
-    const data = await response.json();
-    return json({ data });
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return json(
-      {
-        data: [],
-        message: "Error fetching data from API",
-      },
-      { status: 500 },
-    );
+  function isValidLatitude(lat: string) {
+    const num = parseFloat(lat);
+    return !isNaN(num) && num >= -90 && num <= 90;
   }
+
+  function isValidLongitude(lng: string) {
+    const num = parseFloat(lng);
+    return !isNaN(num) && num >= -180 && num <= 180;
+  }
+
+  if (
+    !isValidLatitude(southLat) ||
+    !isValidLatitude(northLat) ||
+    !isValidLongitude(westLng) ||
+    !isValidLongitude(eastLng)
+  ) {
+    throw new Error("Invalid bounds provided");
+  }
+
+  const queryParams = new URLSearchParams({
+    wgs84CenterPoint: `${encodeURIComponent(southLat)}:${encodeURIComponent(northLat)}:${encodeURIComponent(westLng)}:${encodeURIComponent(eastLng)}:WGS84`,
+    redListStatusId: "MX.iucnEN,MX.iucnCR,MX.iucnVU,MX.iucnNT",
+    pageSize: "10",
+    access_token: accessToken,
+  });
+
+  const api = `https://api.laji.fi/v0/warehouse/query/unit/list?${queryParams.toString()}`;
+
+  const response = await fetch(api);
+
+  if (!response.ok) {
+    console.error("Network response was not ok.");
+    throw new Error("Failed to fetch data from Laji API");
+  }
+  const data = await response.json();
+  return data;
 };
