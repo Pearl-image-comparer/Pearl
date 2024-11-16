921f6791b974e81eadd41dcb6f9cc8eaafbdc803
unknown <jonttis@gmail.com>
2024-11-16 16:29:08 +0200

    refactor: seperate MapBounds to own component

--- app/components/map/_MapComponent.client.tsx
+++ app/components/map/_MapComponent.client.tsx
@@ -12,7 +12,6 @@ import {
   LayersControl,
   MapContainer,
   TileLayer,
-  useMap,
   WMSTileLayer,
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
@@ -20,58 +19,7 @@ import Controls from "~/components/controls/Controls";
 import SpeciesLayer from "./layers/SpeciesLayer";
 import ReportLayer from "./layers/ReportLayer";
 import ConservationLayer from "./layers/ConservationLayer";
-import { useEffect, useState } from "react";
-
-function MapBounds() {
-  const map = useMap();
-  const [hasFetchedInitially, setHasFetchedInitially] = useState(false);
-
-  useEffect(() => {
-    const handleMoveEnd = () => {
-      const newBounds = map.getBounds();
-      const northEast = newBounds.getNorthEast();
-      const southEast = newBounds.getSouthEast();
-      const northWest = newBounds.getNorthWest();
-
-      const northLat = northEast.lat;
-      const southLat = southEast.lat;
-      const eastLng = northEast.lng;
-      const westLng = northWest.lng;
-      const bounds = `${southLat}:${northLat}:${westLng}:${eastLng}`;
-
-      const url = `/lajidata?bounds=${encodeURIComponent(bounds)}`;
-
-      fetch(url)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(
-              "Network response was not ok: " + response.statusText,
-            );
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
-    };
-
-    map.on("moveend", handleMoveEnd);
-
-    if (!hasFetchedInitially) {
-      handleMoveEnd();
-      setHasFetchedInitially(true);
-    }
-
-    return () => {
-      map.off("moveend", handleMoveEnd);
-    };
-  }, [map]);
-
-  return null;
-}
+import MapBounds from "./MapBounds";
 
 export default function MapComponent() {
   const center: L.LatLngExpression = [61.4978, 23.761];
