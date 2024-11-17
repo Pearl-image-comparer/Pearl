dcf58756583fba3c3d7d946b4135e83a0b479175
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-17 19:15:26 +0200

    refactor(menu): simplify container logic

--- app/components/controls/drawer/Drawer.tsx
+++ app/components/controls/drawer/Drawer.tsx
@@ -1,48 +1,58 @@
 import {
     Drawer,
     styled,
+    Box,
     IconButton,
     useMediaQuery,
     useTheme,
+    List,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/MenuOutlined";
+import CloseIcon from "@mui/icons-material/Close";
 import { useState } from "react";
 
-const DrawerContainer = styled("div")(({ theme }) => ({
-    position: "absolute",
-    zIndex: 1000,
+//TODO descide the correct width of drawer
+const DrawerWidth = 250;
+
+//TODO to be removed once the correct opening logic is implemented
+//Button container for the temporary opening logic for the drawer
+const ButtonContainer = styled("div")(({ theme }) => ({
+    position: "fixed",
+    zIndex: 900,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     [theme.breakpoints.down("sm")]: {
         left: "50%",
         bottom: theme.spacing(2),
         transform: "translateX(-50%)",
     },
-    [theme.breakpoints.up("md")]: {
+    [theme.breakpoints.up("sm")]: {
         left: theme.spacing(2),
         top: "50%",
         transform: "translateY(-50%)",
     },
 }));
 
-const ButtonContainer = styled("div")({
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-});
-
 export default function MenuDrawer() {
-    const [drawerOpen, setDrawerOpen] = useState(false);
+    /**
+     * TODO isDrawerOpen and toggleDrawer propably need to be moved to a parent component
+     * once additional logic is implemented for drawer opening, like satellite comparison
+     */
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    // Toggle function to open/close the drawer
+    const toggleDrawer = (open: boolean) => {
+        setIsDrawerOpen(open);
+    };
+
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-    const toggleDrawer = (open: boolean) => () => {
-        setDrawerOpen(open);
-    };
     return (
-        <DrawerContainer>
-            {/* TODO remove this demo button that opens the drawer */}
+        <>
             <ButtonContainer>
                 <IconButton
-                    onClick={toggleDrawer(true)}
+                    onClick={() => toggleDrawer(true)}
                     color="primary"
                     sx={{
                         width: 56,
@@ -57,16 +67,42 @@ export default function MenuDrawer() {
                     <MenuIcon />
                 </IconButton>
             </ButtonContainer>
+
             <Drawer
+                variant="persistent"
                 anchor={isMobile ? "bottom" : "left"}
-                open={drawerOpen}
-                onClose={toggleDrawer(false)}
+                open={isDrawerOpen}
+                sx={{
+                    "& .MuiDrawer-paper": {
+                        width: isMobile ? "100%" : DrawerWidth,
+                        height: isMobile ? "50%" : "100vh", //TODO decide the drawer height on mobile
+                        bottom: isMobile ? 0 : "unset",
+                        left: isMobile ? "unset" : 0,
+                    },
+                }}
             >
-                <div style={{ width: isMobile ? "100%" : 250, padding: 16 }}>
+                <Box
+                    sx={{
+                        padding: 2,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                    }}
+                >
+                    <IconButton
+                        onClick={() => toggleDrawer(false)}
+                        sx={{
+                            alignSelf: "flex-end",
+                            marginBottom: 2,
+                        }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+
                     <h3>Menu</h3>
                     <p>Drawer Content</p>
-                </div>
+                </Box>
             </Drawer>
-        </DrawerContainer>
+        </>
     );
 }
