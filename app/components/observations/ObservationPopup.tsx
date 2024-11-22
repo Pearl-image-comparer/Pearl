15158a9374506ad7fa179d2bf0467f4d1bb7dfe7
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-11-22 18:59:51 +0200

    feat: create custom popup for observation and sighting

---
+++ app/components/observations/ObservationPopup.tsx
@@ -0,0 +1,45 @@
+import { Stack, styled, Typography } from "@mui/material";
+import { Popup } from "react-leaflet";
+import { Observation } from "./ReportDialog";
+import { useEffect, useState } from "react";
+import PlaceIcon from "@mui/icons-material/Place";
+
+interface ObservationPopupProps {
+  observation: Observation;
+}
+
+export default function ObservationPopup({ observation }: ObservationPopupProps) {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (observation.picture) {
+      const url = URL.createObjectURL(observation.picture);
+      setImageUrl(url);
+
+      // Clearing to avoid memory leak
+      return () => URL.revokeObjectURL(url);
+    }
+  }, [observation.picture])
+
+  const StyledImage = styled("img")({
+    width: "100%",
+    borderRadius: "0.5rem",
+    marginTop: "1rem"
+  });
+
+  return (
+    <Popup>
+      <Typography variant="h5">{observation.title}</Typography>
+      <Stack direction="row" alignItems="center" spacing={0.5}>
+        <PlaceIcon />
+        <Typography>{`${observation.longitude}, ${observation.latitude}`}</Typography>
+      </Stack>
+      {imageUrl && <StyledImage src={imageUrl} alt={observation.title} />}
+      <Typography>{observation.description}</Typography>
+    </Popup>
+  );
+}
+
+
+
+
---
+++ app/components/sightings/SightingPopup.tsx
@@ -0,0 +1,79 @@
+import { Stack, Typography } from "@mui/material";
+import { Popup } from "react-leaflet";
+import { Sighting } from "~/routes/lajidata";
+import ReportProblemIcon from "@mui/icons-material/ReportProblem";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import PlaceIcon from "@mui/icons-material/Place";
+
+interface SightingPopupProps {
+  sighting: Sighting
+}
+
+export default function SightingPopup({ sighting }: SightingPopupProps) {
+  const capitalizeName = (name: string) => {
+    if (name) {
+      return name[0].toUpperCase() + name.slice(1, name.length)
+    }
+    return name;
+  };
+
+  const generateReadableEndangerment = (endangerment: string) => {
+    if (endangerment.includes("MX.iucnCR")) return "Äärimmäisen uhanalaiset";
+    if (endangerment.includes("MX.iucnEN")) return "Erittäin uhanalaiset";
+    if (endangerment.includes("MX.iucnVU")) return "Vaarantuneet";
+    if (endangerment.includes("MX.iucnNT")) return "Silmälläpidettävät";
+    return "Ei tiedossa"
+  }
+
+  return (
+    <Popup>
+      <Typography variant="h5">
+        {capitalizeName(sighting.finnishName)}
+      </Typography>
+      <Typography variant="h6">{sighting.latinName}</Typography>
+      <Stack
+        direction="row"
+        alignItems="center"
+        spacing={0.5}
+        sx={{ marginTop: "0.5rem" }}
+      >
+        <PlaceIcon />
+        <Typography>{sighting.coordinates.toString()}</Typography>
+      </Stack>
+      <Stack direction="row" alignItems="center" spacing={0.5}>
+        <ReportProblemIcon />
+        <Typography>
+          {generateReadableEndangerment(sighting.endangerment)}
+        </Typography>
+      </Stack>
+      <Stack direction="row" alignItems="center" spacing={0.5}>
+        <CalendarMonthIcon />
+        <Typography >{sighting.sightingTime}</Typography>
+      </Stack>
+    </Popup>
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
