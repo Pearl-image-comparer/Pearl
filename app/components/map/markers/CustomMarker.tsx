9f11063ef69a64a49808478a0a711fcec907677a
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-11-22 14:03:10 +0200

    feat: use custom markers

--- app/components/map/markers/CustomMarker.tsx
+++ app/components/map/markers/CustomMarker.tsx
@@ -1,28 +1,48 @@
 import { Icon, LatLngExpression } from "leaflet";
 import { Marker } from "react-leaflet";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
+import observationMarker from "~/assets/observation_pin.png";
+import sightingBlackMarker from "~/assets/sighting_pin_black.png";
+import sightingRedMarker from "~/assets/sighting_pin_red.png";
+import sightingOrangeMarker from "~/assets/sighting_pin_orange.png";
+import sightingYellowMarker from "~/assets/sighting_pin_yellow.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 
 interface CustomMarkerProps {
   position: LatLngExpression;
+  variant?: "observation" | "black" | "red" | "orange" | "yellow";
 }
 
-// TODO: Render different markers based on props
-export default function CustomMarker({ position }: CustomMarkerProps) {
+export default function CustomMarker({
+  position,
+  variant = "observation",
+}: CustomMarkerProps) {
+  const determineMarker = () => {
+    switch (variant) {
+      case "black":
+        return sightingBlackMarker;
+      case "red":
+        return sightingRedMarker;
+      case "orange":
+        return sightingOrangeMarker;
+      case "yellow":
+        return sightingYellowMarker;
+      default:
+        return observationMarker
+    }
+  };
+
   return (
     <Marker
       position={position}
       icon={
         new Icon({
-          iconUrl: markerIcon,
-          iconRetinaUrl: markerIcon2x,
-          iconSize: [25, 41],
-          iconAnchor: [12.5, 41],
+          iconUrl: determineMarker(),
+          iconSize: [41, 41],
+          iconAnchor: [20.5, 41],
           shadowUrl: markerShadow,
           shadowRetinaUrl: markerShadow,
           shadowSize: [41, 41],
-          shadowAnchor: [12.5, 41],
+          shadowAnchor: [14, 41],
         })
       }
     />
