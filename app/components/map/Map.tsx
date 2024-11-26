95244c9855011f76044bda7b47085a280fa9a3d2
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2024-11-26 14:55:23 +0200

    fix: force CSR

--- app/components/map/Map.tsx
+++ app/components/map/Map.tsx
@@ -1,18 +1,9 @@
-import { lazy, Suspense, useEffect, useState } from "react";
+import { lazy, Suspense } from "react";
 
 // Lazy loading to ensure the browser environment is available before render.
 const LazyMap = lazy(() => import("./_MapComponent.client"));
 
 export default function Map() {
-  const [isClient, setIsClient] = useState(false);
-
-  // Ensure rendering only in client
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) return null;
-
   return (
     // TODO: Add proper fallback
     <Suspense fallback={<p>Lazy loading...</p>}>
--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import {
   Links,
   Meta,
@@ -8,20 +8,36 @@ import {
 } from "@remix-run/react";
 import "~/main.css";
 import { lightTheme } from "./theme";
+import { useEffect, useState } from "react";
+
+function Layout() {
+  const [isClient, setIsClient] = useState(false);
+
+  // Ensure rendering only in client
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  // Due to Material UI and Emotion we can't render anything on the server.
+  if (!isClient) return <p>Loading...</p>;
+
+  return <Outlet />;
+}
 
 export default function App() {
   return (
     <ThemeProvider theme={lightTheme}>
-      <html lang="en">
+      <html lang="fi">
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="data:image/x-icon;base64,AA" />
           <Meta />
           <Links />
+          <CssBaseline />
         </head>
         <body>
-          <Outlet />
+          <Layout />
           <ScrollRestoration />
           <Scripts />
         </body>
