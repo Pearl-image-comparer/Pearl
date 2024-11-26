fbc0e4570b09f1fd4a602702ca8da76a414e9a8e
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2024-11-26 16:04:40 +0200

    feat(admin): random cookie secret

--- app/utils/sessions.server.ts
+++ app/utils/sessions.server.ts
@@ -1,13 +1,16 @@
 import { createCookieSessionStorage } from "@remix-run/node";
+import crypto from "crypto";
 
 export default createCookieSessionStorage<{ ok: true }>({
   cookie: {
     name: "__session",
-    secrets: ["r3m1xr0ck5"],
-    sameSite: "lax",
+    secrets: [
+      process.env.COOKIE_SECRET || crypto.randomBytes(16).toString("hex"),
+    ],
+    sameSite: "strict",
     httpOnly: true,
     maxAge: 60 * 15, // 15 minutes TTL.
     secure: true,
-    path: "/",
+    path: "/admin",
   },
 });
