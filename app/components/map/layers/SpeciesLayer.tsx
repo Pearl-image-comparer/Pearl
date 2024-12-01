533a4efa7a14f2f295260fd38c217e310826c182
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-12-01 23:10:33 +0200

    feat: separate colors for two marker clusters

--- app/components/map/layers/SpeciesLayer.tsx
+++ app/components/map/layers/SpeciesLayer.tsx
@@ -3,15 +3,42 @@ import CustomMarker from "~/components/map/markers/CustomMarker";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import { Sighting } from "~/routes/lajidata";
 import SightingPopup from "~/components/sightings/SightingPopup";
+import L from "leaflet";
+import { useTheme } from "@mui/material";
 
 interface SpeciesLayerProps {
   data: Sighting[];
 }
 
 export default function SpeciesLayer({ data }: SpeciesLayerProps) {
+  const theme = useTheme();
+
+  const createClusterCustomIcon = function (cluster: {
+    getChildCount: () => number;
+  }) {
+    return L.divIcon({
+      html: `<span style="
+        background-color: ${theme.palette.common.white};
+        color: ${theme.palette.primary.main};
+        border-radius: 50%;
+        height: 2rem;
+        width: 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        opacity: 90%;
+        border: 0.15rem solid ${theme.palette.primary.main};
+        font-weight: bold;">
+        ${cluster.getChildCount()}
+      </span>`,
+      className: "custom-marker-cluster",
+      iconSize: L.point(33, 33, true),
+    });
+  };
+
   return (
     <LayerGroup>
-      <MarkerClusterGroup>
+      <MarkerClusterGroup iconCreateFunction={createClusterCustomIcon}>
         {data &&
           data.map((s, index) => (
             <CustomMarker
