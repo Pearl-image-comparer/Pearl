b5f0be412de597a9e846ede7f44140cbe3649068
Teemu Tontti <teemu.tontti.tt@gmail.com>
2024-12-01 23:03:50 +0200

    feat: add data loading indicator

---
+++ app/components/controls/indicators/ProgressIndicator.tsx
@@ -0,0 +1,27 @@
+import { Alert, Box, CircularProgress, styled } from "@mui/material";
+
+interface ProgressIndicatorProps {
+  text: string;
+}
+
+export default function ProgressIndicator({ text }: ProgressIndicatorProps) {
+  const StyledBox = styled(Box)({
+    display: "flex",
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "end",
+    paddingRight: "0.9rem",
+    width: "100%",
+    top: "4rem",
+    zIndex: 1000,
+    left: 0,
+  });
+
+  return (
+    <StyledBox>
+      <Alert severity="info" icon={<CircularProgress size={20} />} sx={{ bgcolor: "background.paper", color: "text.primary" }}>
+        {text}
+      </Alert>
+    </StyledBox>
+  );
+}
