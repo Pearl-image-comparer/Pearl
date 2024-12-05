4aac89b7d9847d633035fc9dfb48a7c094836df7
Jarkko Kuukkanen <jarkko.kuukkanen@tuni.fi>
2024-12-05 18:04:13 +0200

    feat: title

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
+import type { MetaFunction } from "@remix-run/node";
 import Map from "~/components/map/Map";
+
+export const meta: MetaFunction = () => [{ title: "Pearl" }];
 
 export default function Index() {
   return <Map />;
